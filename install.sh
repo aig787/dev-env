4099508ec150aeaccbecb4e0a4f3741b9715d684
Aaron Griffin <aig787@gmail.com>
2021-02-12 17:57:51 -0800

    Be smarter about fonts

--- install.sh
+++ install.sh
@@ -1,14 +1,25 @@
+mkdir -p .workdir
+cd .workdir
+
 # Install fonts
-git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git
+NERDFONTS_VERSION="v2.1.0"
+FONTS=(Hack Meslo)
+mkdir -p patched-fonts
+for font in "${FONTS[@]}"
+do
+    curl -Lo $font.zip https://github.com/ryanoasis/nerd-fonts/releases/download/$NERDFONTS_VERSION/$font.zip
+    unzip $font.zip -d patched-fonts
+done
+curl -Lo nerd_install.sh https://raw.githubusercontent.com/ryanoasis/nerd-fonts/v2.1.0/install.sh | bash -s
+
+
 git clone https://github.com/gpakosz/.tmux.git
-
-nerd-fonts/install.sh Hack
-nerd-fonts/install.sh Meslo
-
-rm -rf nerd-fonts
 
 # Install vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-curl -fsSL https://starship.rs/install.sh | bash -s -- --yes
+curl -fsSL https://starship.rs/install.sh | sudo bash -s -- --yes
+
+cd ..
+rm -rf .workdir
